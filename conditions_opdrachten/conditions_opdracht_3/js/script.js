c3cbe1cbce4162ea231411dbba30372eed17cd40
LucWendel <lucwendel2002@gmail.com>
2019-11-25 15:11:40 +0100

    aLStu BLieFGT

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,16 @@
-//javascript code
+
+function check() {
+    var saldo = document.getElementById("saldo").value;
+
+    if(saldo == 500) {
+        document.getElementById("tekst").innerHTML = "Baas is weer blij met je jongedame!";
+    }
+
+    else if (saldo < 500) {
+        document.getElementById("tekst").innerHTML = "Je bent tering arm kom maar terug als je geld hebt minderwaardige kech!";
+    }
+
+    else {
+        document.getElementById("tekst").innerHTML = "TERING RIJK";
+    }
+}
