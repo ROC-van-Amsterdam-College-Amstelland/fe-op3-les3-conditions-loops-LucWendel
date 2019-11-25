dc016052509d5bbaa54bfd746878926d6769238d
LucWendel <lucwendel2002@gmail.com>
2019-11-25 14:56:44 +0100

    Hier is mijn tweede opdracht leek wel veel op opdracht 1 hoor

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,14 @@
-//javascript code
+
+
+function check() {
+    var saldo = document.getElementById("saldo").value;
+
+    if(saldo<25) {
+        document.getElementById("tekst").innerHTML = "Je bent tering arm kom maar terug als je geld hebt minderwaardige kech!" ;
+    }
+    else {
+        document.getElementById("tekst").innerHTML = "U heeft veel geld chapeau!";
+    }
+    
+
+}
