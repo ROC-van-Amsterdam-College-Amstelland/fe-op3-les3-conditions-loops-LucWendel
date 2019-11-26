49ffd9acb97eec15c4285c447662ab1794578649
LucWendel <lucwendel2002@gmail.com>
2019-11-26 13:18:27 +0100

    opdracht 6

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -3,6 +3,6 @@ calculate = function()
 {
     var resources = document.getElementById('a1').value;
     var minutes = document.getElementById('a2').value; 
-    document.getElementById('a3').value = parseInt(resources)*parseInt(minutes);
+    document.getElementById('a3').value = parseInt(resources)+parseInt(minutes);
      
    }
