fa56e5139e4f13eb06e9af07736a99ab9252d9d6
LucWendel <lucwendel2002@gmail.com>
2019-11-26 13:35:41 +0100

    iets geprobeerd werkt niet goed

--- conditions_opdrachten/conditions_opdracht_7/js/script.js
+++ conditions_opdrachten/conditions_opdracht_7/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+innit = function() { 
+    var udata = 2;
+    var element = document.getElementById("centerbox1");
+    if (udata >= 70)
+        element.style.backgroundColor = 'white';
+    else if (udata >= 51 && udata.value <70)  
+        element.style.backgroundColor = 'green'; 
+    else if (udata <=50)  
+        element.style.backgroundColor = 'red';
+}
