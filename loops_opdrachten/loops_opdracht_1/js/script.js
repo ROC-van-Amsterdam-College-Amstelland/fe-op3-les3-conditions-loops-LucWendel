2e63b2b32f0080ab792c2f88001865f3bcec4f50
LucWendel <lucwendel2002@gmail.com>
2019-11-26 14:28:29 +0100

    reeeeeeeeeeeee

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -3,6 +3,6 @@ var getallen = ["1", "2", "3", "4", "5"];
 var text = "";
 var i;
 for (i = 0; i < getallen.length; i++) {
-  text += cars[i] + "<br>";
+    text += getallen[i] + "<br>";
 }
 document.getElementById("getallen").innerHTML = text;
