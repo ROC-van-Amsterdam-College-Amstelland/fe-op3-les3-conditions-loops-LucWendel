8b193e7edde1e90d6aeba0840e21fc2493803d89
LucWendel <lucwendel2002@gmail.com>
2019-11-26 13:41:17 +0100

    opdracht 1 gedaan

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,8 @@
 //javascript code
+var getallen = ["1", "2", "3", "4", "5"];
+var text = "";
+var i;
+for (i = 0; i < getallen.length; i++) {
+  text += cars[i] + "<br>";
+}
+document.getElementById("getallen").innerHTML = text;
