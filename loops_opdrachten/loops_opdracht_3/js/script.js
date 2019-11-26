f00ece63a0d66032eb6758a35e634457653d79de
LucWendel <lucwendel2002@gmail.com>
2019-11-26 14:41:51 +0100

    ola senor

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,12 @@
-//javascript code
+
+var text = "";
+var i;
+for (i = 1; i <= 50; i++) {
+    text += i + "<br>";
+}
+document.getElementById("getallen").innerHTML = text;
+
+for(i = 20; i <= 40; i++){
+    text += "ditzijnmooiegetallen "
+}
+document.getElementById("getallen").innerHTML = text;
