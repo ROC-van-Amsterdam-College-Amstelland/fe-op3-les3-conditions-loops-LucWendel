112775d8f1e52af6fdd6baf77edbc946a1fb71b0
LucWendel <lucwendel2002@gmail.com>
2019-11-26 12:46:05 +0100

    opdracht 5 is vandaag gedaan

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,8 @@
-//javascript code
+
+calculate = function()
+{
+    var resources = document.getElementById('a1').value;
+    var minutes = document.getElementById('a2').value; 
+    document.getElementById('a3').value = parseInt(resources)*parseInt(minutes);
+     
+   }
