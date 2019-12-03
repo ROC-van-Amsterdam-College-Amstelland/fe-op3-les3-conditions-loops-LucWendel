10ba40fcf87860e1a0604a71f283240303c85060
LucWendel <luc.wendel@student.rocva.nl>
2019-12-03 16:10:56 +0100

    opdracht 4 werkt nu wel

--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,3 +1,14 @@
-for (var i = 0; i >= +1000; i++) {
-    console.log(i);
+function check() {
+    var tekst = "";
+    for (i = 0; i <= 1000; i++) {
+        if (i == 666) {
+            tekst += i + " kiekeboe<br>";
+        } else {
+            tekst += i + "<br>"
+        }
+
+        console.log(i);
+    }
+    document.getElementById("tekst").innerHTML = tekst;
+
 }
